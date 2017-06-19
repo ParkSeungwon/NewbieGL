70467fa4de9c70cfaa90115355369702b3d1dfb3
zezeon <zezeon@msn.com>
2017-06-19 17:29:46 +0900

    +

--- src/val2.cpp
+++ src/val2.cpp
@@ -1,9 +1,10 @@
 #include<chrono>
 #include<thread>
 #include<iostream>
+#include<valarray>
 #include"glutil.h"
 #include"globj.h"
-#include"spring.h"
+#include"plot.h"
 using namespace std;
 extern Matrix<float> KeyBindMatrix;
 
@@ -13,35 +14,31 @@ int main(int ac, char** av)
 	GLFWwindow* window = glfwCreateWindow(1024, 768, "Color Cube", NULL, NULL);
 	if (!glinit(window)) return -1;
 
+	auto th = arange(0, 0.01, M_PI * 8);
+	valarray<float> x = cos(th), y = sin(th), z = th / 9.2f;
+	vector<Matrix<float>> v;
+	for(int i=0; i<x.size(); i++) v.push_back({x[i], y[i], z[i]});
+
 	Matrix<float> m{4,4};
 	GLObject obj3d;
-	obj3d.read_obj_file(av[1]);
-	obj3d.matrix(m.glrotateY(-M_PI/2) * m.glrotateX(-M_PI/2) * m.glscale(0.8,0.8,0.8));
+	obj3d.vertexes(v);
+	obj3d.colors(vector<Matrix<float>>{x.size(), {0,1,0}});
+	obj3d.mode(GL_LINE_STRIP);
 	
-//	obj3d.subdiv_triangle();
-//	obj3d.subdiv_triangle();
-//	obj3d.butterfly();
-//	obj3d.butterfly();
-	obj3d.texture_file(av[2]);
 	GLObjs stage;
 	stage += obj3d;
 	stage.transfer_all();
-	float th = 0;
 	Matrix<float> lt {
-		{0.1, 0.1, 0.1, 1}, //ambient
-		{0.5, 0.5, 0.5, 0.5}, //diffuse
+		{1, 1, 1, 1}, //ambient
+		{1, 1, 1, 1}, //diffuse
 		{1, 1, 1, 1}, //specular
-		{3, 3, -3, 1} //position 1 means a point 0 means a vector light
+		{0, 0, 0, 0} //position 1 means a point 0 means a vector light
 	};
-	SpringModel3D sp;
-	float t = 0;
+	stage.light(lt);
 	while (!glfwWindowShouldClose(window)) {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		
-		sp.time_pass(0.3 * sin(0.2*t), 0, 0.1 * cos(0.2*t));
-		t+= 0.1;
-		cout << sp.x << ' ' << sp.y << ' ' << sp.z << endl;
-		stage.matrix(m.glprojection(-1,1,-1,1,-1,1)*KeyBindMatrix *  m.gltranslate(sp.x, sp.y, sp.z) * stage[0]);
+		stage.matrix(m.glortho(-1.5, 1.5, -1.5, 1.5, -2,2) * KeyBindMatrix * stage[0]);
 		stage(0);
 
 		glfwSwapBuffers(window);
