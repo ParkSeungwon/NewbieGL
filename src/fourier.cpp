4d89827aed8b6208dd1e3ded913b62f10ed6d5fa
zezeon <zezeon@msn.com>
2017-08-07 15:35:03 +0900

    +

--- src/fourier.cpp
+++ src/fourier.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int main()
 {
-	auto x = linspace(0, 2.*3.14, 1000);
-	auto y = sin(3.14f*x);
+	auto x = linspace(0, 3.14, 100);
+	valarray<float> y = sin(x) + sin(2.f*x) + sin(3.f*x);//auto -> bug, must define valarray
 
-	valarray<float> r(1000);
-	auto f = linspace(0, 10, 1000);
-
-	for(int i=0; i<1000; i++) r[i] = abs(DFT(y, f[i]));
+	int i=0;
+	auto f = linspace(-1, 1, 1000);
+	auto r = f;
+	for(auto a : f) r[i++] = abs(DFT(y, a));
 	plot(f, r);
 }
 
